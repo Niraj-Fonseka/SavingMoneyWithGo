568ebfec088ecdcf7ae473dd78850520294386bb
Niraj-Fonseka <fonseka.live@utexas.edu>
2018-11-05 21:01:49 -0600

    Adding the singleflight api package

--- DataSource/datasource.go
+++ DataSource/datasource.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
+var cost int64
+
 func main() {
-	http.HandleFunc("/", helloWorldHandler)
+
+	http.HandleFunc("/fetch_data", helloWorldHandler)
 	http.ListenAndServe(":8001", nil)
 }
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world Datasource!")
+	data := GetData()
+	io.WriteString(w, data)
+
+}
+
+func GetData() string {
+	time.Sleep(5 * time.Second)
+	cost += 100
+	return fmt.Sprintf("Fetch called : %d", cost)
 }
